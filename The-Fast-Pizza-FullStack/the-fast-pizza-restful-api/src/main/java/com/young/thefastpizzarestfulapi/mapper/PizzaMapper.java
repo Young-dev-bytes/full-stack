7eb1ecf46c1a28c59f20851b7247c23ab0be4fbd
Young <18855490639@163.com>
2024-04-27 19:54:40 +0800

    test

---
+++ The-Fast-Pizza-FullStack/the-fast-pizza-restful-api/src/main/java/com/young/thefastpizzarestfulapi/mapper/PizzaMapper.java
@@ -0,0 +1,8 @@
+package com.young.thefastpizzarestfulapi.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.young.thefastpizzarestfulapi.entity.UserDo;
+
+public interface UserMapper extends BaseMapper<UserDo> {
+
+}
